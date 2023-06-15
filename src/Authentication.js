82ad8967d75ea254ab04621efcf75724a4c9a30a
Faruk Khan <Faruk.Khan@matellio.com>
2023-06-15 16:37:23 +0530

    add css

---
+++ src/Authentication.js
@@ -0,0 +1,71 @@
+import React from "react";
+import "./App.css";
+import "@aws-amplify/ui-react/styles.css";
+
+import { Authenticator, ThemeProvider, useTheme } from "@aws-amplify/ui-react";
+import App from "./App";
+
+const Authentication = ({ signOut }) => {
+  const { tokens } = useTheme();
+  const theme = {
+    name: "Auth Example Theme",
+    tokens: {
+      colors: {
+        background: {
+          primary: {
+            value: tokens.colors.neutral["90"].value,
+          },
+          secondary: {
+            value: tokens.colors.neutral["100"].value,
+          },
+        },
+        font: {
+          interactive: {
+            value: tokens.colors.white.value,
+          },
+        },
+        brand: {
+          primary: {
+            10: tokens.colors.teal["100"],
+            80: tokens.colors.teal["40"],
+            90: tokens.colors.teal["20"],
+            100: tokens.colors.teal["10"],
+          },
+        },
+      },
+      components: {
+        tabs: {
+          item: {
+            _focus: {
+              color: {
+                value: tokens.colors.white.value,
+              },
+            },
+            _hover: {
+              color: {
+                value: tokens.colors.yellow["80"].value,
+              },
+            },
+            _active: {
+              color: {
+                value: tokens.colors.white.value,
+              },
+            },
+          },
+        },
+      },
+    },
+  };
+
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+        <Authenticator>
+          <App />
+        </Authenticator>
+      </ThemeProvider>
+    </>
+  );
+};
+
+export default Authentication;
